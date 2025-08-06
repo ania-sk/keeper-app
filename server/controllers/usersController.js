ec960a0d539aa988abb5fbf05cd59e6c448aa7a8
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-08-06 10:46:03 +0200

    create usersController with registerUser logic

---
+++ server/controllers/usersController.js
@@ -0,0 +1,30 @@
+import bcrypt from "bcrypt";
+import pool from "../db.js";
+
+export async function registerUser(req, res) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "Incorrect data" });
+  }
+
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (result.rows.length > 0) {
+      return res.status(409).json({ error: "Email already exists" });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    await pool.query(
+      "INSERT INTO users (email, password_hash) VALUES ($1, $2)",
+      [email, passwordHash]
+    );
+
+    res.ststus(201).json({ message: "Registration completed successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+}
