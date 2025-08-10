cdd3324bbea991af18037411c8947dbb7e2fdf93
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-08-10 15:08:07 +0200

    create logout button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import HighlightIcon from "@mui/icons-material/Highlight";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("accessToken");
+    navigate("/login");
+  }
+
   return (
     <header>
       <h1>
         <HighlightIcon />
         Keeper
       </h1>
+      <button onClick={handleLogout}>Logout</button>
     </header>
   );
 }
