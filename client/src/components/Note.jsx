3004095950998260e541ca63e762c920a2ab0135
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-08-12 13:22:59 +0200

    add Edit button to note area

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 function Note(props) {
-  function handleClick() {
+  function handleDelete() {
     props.onDelete(props.id);
   }
+
+  function handleEdit() {}
 
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>
+      <button onClick={handleDelete}>
         <DeleteIcon />
+      </button>
+      <button onClick={handleEdit}>
+        <EditIcon />
       </button>
     </div>
   );
