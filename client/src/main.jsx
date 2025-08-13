395e14ff1382134f93389c8d1d331f8504608d6e
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-08-13 16:40:49 +0200

    fix: restore BrowserRouter to enable routing context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <AppRoutes />
-  </AuthProvider>
+  <BrowserRouter>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+  </BrowserRouter>
 );
