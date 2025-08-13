10cd342eca931521f10c51b83714ac4fdaaacc18
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-08-13 16:13:34 +0200

    chore: wrap app in AuthProvider to enable global auth context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import { AuthProvider } from "./context/AuthContext";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <AuthProvider>
     <AppRoutes />
-  </BrowserRouter>
+  </AuthProvider>
 );
