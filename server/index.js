792aafc2310b59f049e9a1af51797d4d7e8ed5ac
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-08-19 15:22:04 +0200

    update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://keeper-app-frontend-m2n9.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/notes", notesRoutes);
 app.use("/api", usersRouter);
