f48670a53612ab7201b07e90e2d0589a63339e0b
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-08-19 15:26:40 +0200

    update index.js

--- server/index.js
+++ server/index.js
@@ -18,6 +18,6 @@ app.use(express.json());
 app.use("/api/notes", notesRoutes);
 app.use("/api", usersRouter);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 10000, "0.0.0.0", () => {
   console.log(`server is running on port ${process.env.PORT}`);
 });
