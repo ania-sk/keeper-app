17eeb3f86a5db85a5179cfdff2b6fedab2051064
ania-sk <127092221+ania-sk@users.noreply.github.com>
2026-05-21 10:09:27 +0200

    feat(router): add /privacy-policy and /terms routes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useState, useEffect } from "react";
 import App from "../App";
 import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
+import PrivacyPolicy from "../components/legal/PrivacyPolicy";
+import TermsOfService from "../components/legal/TermsOfService";
 import { useAuth } from "../context/AuthContext";
 
 function AppRoutes() {
@@ -17,6 +18,8 @@ function AppRoutes() {
       />
       <Route path="/login" element={<LoginForm />} />
       <Route path="/register" element={<RegisterForm />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms" element={<TermsOfService />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
